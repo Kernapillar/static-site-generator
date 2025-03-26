cf8e09c88f4d0b609cc076fcad7455faf90008e3
Alexander Kern <alexander.t.kern@gmail.com>
2025-03-26 10:42:25 -0500

    Markdown Converter and Testing

---
+++ src/markdown_converter.py
@@ -0,0 +1,20 @@
+from textnode import TextType, TextNode
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type): 
+    new_nodes = []
+    for node in old_nodes: 
+        if node.text_type != TextType.TEXT: 
+            new_nodes.append(node)
+        else: 
+            split = node.text.split(delimiter)
+            if len(split) == 2: 
+                raise Exception("Invalid Markdown Syntax: matching delimiter not found")
+            for i in range(len(split)):
+                if split[i] == "": 
+                    continue
+                if i % 2 == 1: 
+                    new_nodes.append(TextNode(split[i], text_type))
+                else: 
+                    new_nodes.append(TextNode(split[i], TextType.TEXT))
+    return new_nodes
+            
