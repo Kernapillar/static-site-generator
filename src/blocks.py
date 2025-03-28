0cccbee8dcdfe9d38a87218079d50f10ff487192
Alexander Kern <alexander.t.kern@gmail.com>
2025-03-28 11:32:18 -0500

    block to block type with tests

--- src/blocks.py
+++ src/blocks.py
@@ -1,4 +1,11 @@
-
+from enum import Enum
+class BlockType(Enum): 
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDEREDLIST = "unordered list"
+    ORDEREDLIST = "ordered list"
 
 def markdown_to_blocks(markdown): 
     blocks = markdown.split("\n\n")
@@ -11,3 +18,22 @@ def markdown_to_blocks(markdown):
         if len(block) > 0: 
             result.append(block)
     return result
+
+def block_to_block_type(block): 
+    if block.startswith("#"): 
+        return BlockType.HEADING
+    if block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    lines = block.split("\n")
+    if all(line.startswith(">") for line in lines): 
+        return BlockType.QUOTE
+    if all(line.startswith("- ") for line in lines): 
+        return BlockType.UNORDEREDLIST
+    ordered = True
+    for i in range(len(lines)): 
+        if lines[i].startswith(f"{i + 1}. ") != True: 
+            ordered = False
+    if ordered: 
+        return BlockType.ORDEREDLIST
+    else: 
+        return BlockType.PARAGRAPH
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -69,6 +69,70 @@ class TestMarkdownConverter(unittest.TestCase):
             ],
         )
 
+    def test_block_to_blocktype_heading(self): 
+        block = "# This is a Heading block"
+        type = block_to_block_type(block)
+        self.assertEqual(type, BlockType.HEADING)
+
+    def test_block_to_blocktype_heading2(self): 
+        block = "### This is a Heading block"
+        type = block_to_block_type(block)
+        self.assertEqual(type, BlockType.HEADING)
+
+    def test_block_to_blocktype_heading3(self): 
+        block = "3### This is not a Heading block"
+        type = block_to_block_type(block)
+        self.assertNotEqual(type, BlockType.HEADING)
+
+    def test_block_to_blocktype_code(self): 
+        block = "``` This is a Code block```"
+        type = block_to_block_type(block)
+        self.assertEqual(type, BlockType.CODE)
+
+    def test_block_to_blocktype_code2(self): 
+        block = "`` This is not a Code block``"
+        type = block_to_block_type(block)
+        self.assertNotEqual(type, BlockType.CODE)
+
+    def test_block_to_blocktype_code3(self): 
+        block = "``` This is not a Code block``"
+        type = block_to_block_type(block)
+        self.assertNotEqual(type, BlockType.CODE)
+
+    def test_block_to_blocktype_quote(self): 
+        block = "> this \n> is \n> a \n> quote \n> block"
+        type = block_to_block_type(block)
+        self.assertEqual(type, BlockType.QUOTE)
+
+    def test_block_to_blocktype_quote2(self): 
+        block = "> this \n> is \nnot \n> a \n> quote \n> block"
+        type = block_to_block_type(block)
+        self.assertNotEqual(type, BlockType.QUOTE)
+
+    def test_block_to_blocktype_unordered_list(self): 
+        block = "-  this \n-  is \n-  an \n-  unordered list \n-  block"
+        type = block_to_block_type(block)
+        self.assertEqual(type, BlockType.UNORDEREDLIST)
+
+    def test_block_to_blocktype_unordered_list2(self): 
+        block = "-  this \n-  is \nnot \n-  an \n-  unordered list \n-  block"
+        type = block_to_block_type(block)
+        self.assertNotEqual(type, BlockType.UNORDEREDLIST)
+
+    def test_block_to_blocktype_ordered_list(self): 
+        block = "1. this \n2. is \n3. an \n4. ordered list \n5. block"
+        type = block_to_block_type(block)
+        self.assertEqual(type, BlockType.ORDEREDLIST)
+
+    def test_block_to_blocktype_ordered_list2(self): 
+        block = "1. this \n2. is \n3. not \n4. an \n4. ordered list \n5. block"
+        type = block_to_block_type(block)
+        self.assertNotEqual(type, BlockType.ORDEREDLIST)
+
+    def test_block_to_blocktype_paragraph(self): 
+        block = "This is a paragraph block"
+        type = block_to_block_type(block)
+        self.assertEqual(type, BlockType.PARAGRAPH)
 
 if __name__ == "__main__":
     unittest.main()
