b3b59b4c002e734fe7aea6ace6c8dabaef67793e
Alexander Kern <alexander.t.kern@gmail.com>
2025-03-30 14:53:11 -0500

    recursive call to copy static contents to public

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 from textnode import TextType, TextNode
 from htmlnode import HTMLNode
 from leafnode import LeafNode
@@ -5,20 +7,34 @@ from parentnode import ParentNode
 
 
 def main(): 
-    test_node = TextNode("test Text", TextType.LINK, "https://www.alex.com")
-    print(test_node)
-    html_node = HTMLNode("p", "test value", [], {"test prop key": "test prop value"})
-    print(html_node)
-    parent_node = ParentNode(
-    "p",
-        [
-            LeafNode("b", "Bold text"),
-            LeafNode(None, "Normal text"),
-            LeafNode("i", "italic text"),
-            LeafNode(None, "Normal text"),
-        ],
-    )
+    static_to_public()
 
-    print(parent_node)
+def static_to_public(): 
+    # clear public directory: 
+    if os.path.exists("./public"):
+        shutil.rmtree("./public")
+    os.mkdir("./public")
+
+    # recursively copy everything into public: 
+    def copy_over(current_path=""): 
+        copy_path = os.path.join("static", current_path)
+        target_path = os.path.join("public", current_path)
+        for item in os.listdir(copy_path): 
+            # print(item)
+            item_path = os.path.join(copy_path, item)
+            dest_path = os.path.join(target_path, item)
+            # print('itemPath = '+ item_path)
+            # print('destPath = '+ dest_path)
+            if os.path.isfile(item_path):
+                # print("found an item" + item)
+                shutil.copy(item_path, dest_path)
+            elif os.path.isdir(item_path): 
+                # print("found dir" + item)
+                os.mkdir(dest_path)
+                new_current_path = os.path.join(current_path, item)
+                copy_over(new_current_path)
+
+    copy_over()
+
 
 main()
