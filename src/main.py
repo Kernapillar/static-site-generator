b403f56927dd316f18460025e5b870a477e1c1b2
Alexander Kern <alexander.t.kern@gmail.com>
2025-03-31 15:12:06 -0500

    Updated basepath logic

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import sys
 from generate_page import *
 
 def main(): 
-    basepath = sys.argv[0] if sys.argv[0] != "" else "/"
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
 
     static_to_docs()
     generate_pages_recursive("content", "template.html", "docs", basepath)
